872b5d8346bd61cdef00cf9dfbf7b2a85250578b
jonasfranco91 <100517634+jonasfranco91@users.noreply.github.com>
2022-07-14 20:45:38 -0300

    Add files via upload

---
+++ JJ.js
@@ -0,0 +1,3 @@
+et d = new Date();
+document.body.innerHTML = "<h6>Time right now is:  " +
+ d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds()</h6>"
---
+++ meu primeiro java.java
@@ -0,0 +1,9 @@
+// JavaScript source code
+console.log("meu primeiro programa trabalhando com variaveis ");
+
+const idade = 26;
+const nome = jonas;
+const sobrenome = franco;
+
+
+console.log("meu nome é  "nome);
